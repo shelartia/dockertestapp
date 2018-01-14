052dbb2baf0b23e536ad88da51599f94f41524d2
Artem Sh <artemshelepun@gmail.com>
2018-01-14 22:42:00 +0200

    add jenkins

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ "$ENV"='DEV' ]; then
+if [ "$ENV" = "DEV" ]; then
   echo "Running Development Server"
+  echo "..."
   exec python "identidock.py"
+elif [ "$ENV" = "UNIT" ]; then
+  echo "Running auto tests"
+  exec python "test.py"
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
